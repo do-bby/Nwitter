2ec11f5aad540157ef075f3c6427a0a517bda4ce
do-bby <qkrdbstn25@gmail.com>
2023-12-15 11:44:59 +0900

    tweet

--- src/components/github-btn.tsx
+++ src/components/github-btn.tsx
@@ -1,23 +1,41 @@
+import { GithubAuthProvider, signInWithPopup } from "firebase/auth";
 import {styled} from "styled-components";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const Button = styled.span`
-    backgroud-color : white,
+    margin-top:50px;
+    background-color:white;
     font-weight:600;
+    width:100%;
+    color:black;
     padding: 10px 20px;
-    border-radius:50px
+    border-radius:50px;
     border:0;
     display:flex;
     gap:5px;
     align-items:center;
     justify-content:center;
+    cursor:pointer;
 `;
 
 const Logo = styled.img`
     height:25px;
 `;
 export default function GithubButton(){
+    const navigate = useNavigate();
+    const onClick = async () => {
+        try{
+            const provider = new GithubAuthProvider();
+            await signInWithPopup(auth, provider);
+            // signInWithRedirect 는 깃허브 페이지로 이동하여 로그인
+            navigate("/");
+        } catch(error){
+            console.error(error);
+        }        
+    }
     return(
-    <Button>
+    <Button onClick = {onClick}>
         <Logo src = "/github-logo.svg" />
         Continue With Github
     </Button>   
