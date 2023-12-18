bbeb1a01482579b12513f96768a0a87e3c2bb96d
do-bby <qkrdbstn25@gmail.com>
2023-12-18 13:12:13 +0900

    tweet

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import {getStorage} from "firebase/storage";
+import {getFirestore} from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyDDuCx3OUq_aCUoZxw2HI7xiELRVqzk9Uo",
   authDomain: "nwitter-project-2a282.firebaseapp.com",
@@ -12,3 +14,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
